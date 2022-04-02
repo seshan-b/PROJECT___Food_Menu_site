c54abacde5c08c6c75255c110c533d93781648c1
seshan-b <seshan.b@gmail.com>
2022-04-02 17:30:09 +1300

    Clean up

--- scripts.js
+++ scripts.js
@@ -1,9 +1,7 @@
 /* Navbar */
-
 document.querySelector('.menu').addEventListener('click', () => {
 	document.querySelectorAll('.target-selector').forEach((item) => {
 		item.classList.toggle('change-selector')
 	})
 })
-
 /* End Navbar */
